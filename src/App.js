1544eb840c327526886bcc85fd36fcac4aedb55b
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2025-02-12 00:44:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './components/Form'; // Adjust the path if necessary
+import './index.css'; // Ensure CSS is loaded
+
+function App() {
+  return (
+    <div className="App">
+      <h1><b>Social Media Form</b></h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
